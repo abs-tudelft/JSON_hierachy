5b8c99a1f50fd32e30bab8e8d165cf75f6024ac8
jhaenen <jasper@haenenweb.nl>
2022-11-30 19:06:10 +0100

    Add comments

--- src/main.rs
+++ src/main.rs
@@ -57,9 +57,9 @@ fn main() {
     //  "#;
 
     // Deserialize the JSON string
-    let p = json::parse(data).unwrap();
+    let parsed = json::parse(data).unwrap();
 
-    let (root, _) = analyze_element(&p, 0, 0);
+    let (root, _) = analyze_element(&parsed, 0, 0);
 
     println!("{:?}", root);
 }
@@ -98,6 +98,7 @@ fn analyze_element(element: &json::JsonValue, outer_nesting: u16, inner_nesting:
                         }
                     )
                 ), 
+                // Types don't increase the nesting level
                 inner_nesting
             ),
         // Element has integer type
@@ -111,6 +112,7 @@ fn analyze_element(element: &json::JsonValue, outer_nesting: u16, inner_nesting:
                         }
                     )
                 ), 
+                // Types don't increase the nesting level
                 inner_nesting
             ),
         // Element has boolean type
@@ -124,13 +126,14 @@ fn analyze_element(element: &json::JsonValue, outer_nesting: u16, inner_nesting:
                         }
                     )
                 ), 
+                // Types don't increase the nesting level
                 inner_nesting
             ),
         // Element is an array
         json::JsonValue::Array(arr) => {
             // If the array is empty, return None
             if arr.is_empty() {
-                return (None, inner_nesting);
+                return (None, inner_nesting + 1);
             }
 
             // Get the first element of the array to determine the type of the array
@@ -151,23 +154,31 @@ fn analyze_element(element: &json::JsonValue, outer_nesting: u16, inner_nesting:
                         }
                     )
                 ),
+                // An array increases the inner nesting by 1
                 new_inner_nesting + 1
             )
         },
         // Element is an object
         json::JsonValue::Object(_) => {
-            // Analyze all the records of the object
             let mut children: Vec<Record> = Vec::new();
             let mut new_inner_nesting = Vec::new();
+
+            // Analyze all the records of the object
             for key in element.entries() {
+                // Analyze the record
                 let (child, ret_inner_nesting) = analyze_record(key.0, key.1, outer_nesting + 1, inner_nesting);
+                
+                // Check if the record is not empty
                 match child {
                     Some(JsonComponent::Record(record)) => children.push(record),
                     _ => (),
                 } 
+
+                // Save the inner nesting level of the record
                 new_inner_nesting.push(ret_inner_nesting);
             }
 
+            // Take the maximum inner nesting of the object's records
             let max_inner_nesting = *(new_inner_nesting.iter().max().unwrap());
 
             // Return the object with the children
@@ -181,6 +192,7 @@ fn analyze_element(element: &json::JsonValue, outer_nesting: u16, inner_nesting:
                         }
                     )
                 ),
+                // An object increases the inner nesting by 1
                 max_inner_nesting + 1
             )
         },
